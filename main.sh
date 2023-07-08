243c769d8981a9483d96c3b511e30a3c1553923e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-08 02:02:45 -0700

    1.8.0 Release: After several testing, back to using --notes "$INPUT_DESC" again

--- main.sh
+++ main.sh
@@ -4,4 +4,4 @@
 echo "INFO: INPUT_TAG : '$INPUT_TAG'"
 echo "INFO: INPUT_DESC: '$INPUT_DESC'"
 
-gh release create "$INPUT_TAG" --title "$INPUT_TAG" --notes "'$INPUT_DESC'"
+gh release create "$INPUT_TAG" --title "$INPUT_TAG" --notes "$INPUT_DESC"
